e10326db12092d18e50f26fb206da91a9f00e795
HannibalLecter <brunor_gon_rocha@hotmail.com>
2019-11-18 20:09:46 +0000

    1

---
+++ RottenTabaibos/resources/views/layouts/layout.blade.php
@@ -0,0 +1,13 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RottenTabaibos</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.11.1/css/all.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+@yield('content')
--- RottenTabaibos/resources/views/welcome.blade.php
+++ RottenTabaibos/resources/views/welcome.blade.php
@@ -1,16 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>RottenTabaibos</title>
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.11.1/css/all.css">
-    <link rel="stylesheet" href="css/style.css">
-</head>
+@extends('layouts.layout')
+
+@section('content')
 
 <body>
     <header>
@@ -156,5 +149,6 @@
         </section>
     </main>
 </body>
-
+@endsection
 </html>
+
